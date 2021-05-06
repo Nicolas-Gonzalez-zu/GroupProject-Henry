dc0c6b0fa8b69a51bc2d08666a163738a43cf494
Yilmer Avila <yilme0001@hotmail.com>
2021-05-06 13:55:45 -0500

    create config migration

---
+++ api/models/configuration.js
@@ -0,0 +1,30 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Configuration extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // static associate(models) {
+    //   // define association here
+    // }
+  }
+  Configuration.init(
+    {
+      app_name: DataTypes.STRING,
+      addres: DataTypes.STRING,
+      fiscal_id: DataTypes.STRING,
+      payment_gw_api_key: DataTypes.STRING,
+      transactional_gw_api_key: DataTypes.STRING,
+      secret_key: DataTypes.STRING,
+      logo_url: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Configuration',
+    },
+  );
+  return Configuration;
+};
