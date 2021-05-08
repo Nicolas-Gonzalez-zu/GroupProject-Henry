6c72fc279303f26cbc75da1e893329b273d8d7c3
Nicolas-Gonzalez-zu <nico.gonzalez.sanabria@gmail.com>
2021-05-08 04:03:53 -0300

    Endpoint to upload profile img created

--- api/src/app/routes/front-ofice/index.js
+++ api/src/app/routes/front-ofice/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const Upload = require('../../controllers/avatarUploadsController');
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart();
+
 const customerController = require('../../controllers/customerController');
 
 router.use('/customer', customerController);
@@ -9,5 +13,8 @@ router.use('/customer', customerController);
 router.get('/', (req, res) => {
   res.json({ message: 'FO response' });
 });
+
+router.get('/form', Upload.displayForm);
+router.post('/upload', multipartMiddleware, Upload.upload);
 
 module.exports = router;
