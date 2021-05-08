459285677888ac8bc06be61d32a2b59d82a6aa6d
Nicolas-Gonzalez-zu <nico.gonzalez.sanabria@gmail.com>
2021-05-08 20:46:33 -0300

    Route rdy for profile

--- api/src/app/routes/front-ofice/index.js
+++ api/src/app/routes/front-ofice/index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
-
-
-const Upload = require('../../controllers/avatarUploadsController');
 const multipart = require('connect-multiparty');
-const multipartMiddleware = multipart();
+const Upload = require('../../controllers/avatarUploadsController');
 
+const multipartMiddleware = multipart();
 
 const walletController = require('../../controllers/walletController');
 const budgetController = require('../../controllers/budgetController');
 
 router.use('/wallet', walletController);
-
 
 const customerController = require('../../controllers/customerController');
 const walletControlelr = require('../../controllers/walletController');
@@ -21,7 +18,6 @@ router.use('/customer', customerController);
 router.use('/wallet', walletControlelr);
 
 router.use('/budget', budgetController);
-
 
 router.get('/', (req, res) => {
   res.json({ message: 'FO response' });
