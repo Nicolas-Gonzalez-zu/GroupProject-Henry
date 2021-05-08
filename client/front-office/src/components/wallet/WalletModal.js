74a4481fb5bf750f7edc55882af9b50310ef11be
joacobian21@gmail.com <joacobian21@gmail.com>
2021-05-08 02:59:06 -0300

    adding wallet functionality

---
+++ client/front-office/src/components/wallet/WalletModal.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+const WalletModal = ({ wallet, setWalletHandler, setWalletsHandler, showModal, setShowModal }) => {
+  //   const [showModal, setShowModal] = useState(false);
+
+  const setModalHandler = (e) => {
+    e.preventDefault();
+    setShowModal(!showModal);
+  };
+  return (
+    <div>
+      <Button onClick={setModalHandler} className="btn-success">
+        Add Wallet
+      </Button>
+      <Modal show={showModal}>
+        <Modal.Header>Complete the inputs please</Modal.Header>
+        <Modal.Body>
+          <div className="d-flex flex-column">
+            <input
+              type="text"
+              placeholder="cash..."
+              name="name"
+              onChange={setWalletHandler}
+              value={wallet.name}
+            />
+            <input
+              type="number"
+              placeholder="amount..."
+              name="amount"
+              onChange={setWalletHandler}
+              value={wallet.amount}
+            />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="btn-success" onClick={(e) => setWalletsHandler(e)}>
+            Add Wallet
+          </Button>
+          <Button onClick={setModalHandler}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+export default WalletModal;
