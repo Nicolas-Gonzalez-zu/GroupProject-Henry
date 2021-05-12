75295d37f573a96858152072b80b8a4e6976b32f
decepti0n <jcesar.zaravia@gmail.com>
2021-05-12 18:12:36 -0500

    Registro de primeras modificaciones

--- api/src/app/routes/front-ofice/index.js
+++ api/src/app/routes/front-ofice/index.js
@@ -7,6 +7,7 @@ const budgetController = require('../../controllers/budgetController');
 const customerController = require('../../controllers/customerController');
 const walletControlelr = require('../../controllers/walletController');
 const movementController = require('../../controllers/movementController');
+const transferController = require('../../controllers/transferController');
 
 const router = express.Router();
 
@@ -20,6 +21,7 @@ router.use('/wallet', walletControlelr);
 router.use('/budget', budgetController);
 
 router.use('/movement', movementController);
+router.use('/transfer', transferController);
 
 router.get('/', (req, res) => {
   res.json({ message: 'FO response' });
