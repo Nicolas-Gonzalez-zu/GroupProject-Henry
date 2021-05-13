e1dd647d5e2f2c251655976f4947107b5d9c4d1f
decepti0n <jcesar.zaravia@gmail.com>
2021-05-13 02:21:09 -0500

    Ordenamiento simple de archivos de rutas para front-office y back-office

--- api/src/app/utils/globalCodes.js
+++ api/src/app/utils/globalCodes.js
@@ -2,6 +2,7 @@ const errorCode = Object.freeze({
   INCONSISTENT_DATA: 'Inconsistency was detected in the data received',
   MISSING_ATTRIBUTES: 'The absence of one or more necessary parameters was detected',
   UNFINISHED_OPERATION: 'Operation canceled due to one or more actions not completed successfully',
+  UNAUTHORIZED_OPERATION: 'Access to this endpoint is restricted ',
 });
 
 const statusCode = Object.freeze({
@@ -9,6 +10,7 @@ const statusCode = Object.freeze({
   CREATED: 201,
   EDITED: 204, // Also called no_content
   BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
   INTERNAL_SERVER_ERROR: 500,
 });
 
