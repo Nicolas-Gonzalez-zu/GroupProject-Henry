2028ad3e78d66dd2fe721e96b0b78c2d57d3e2c0
Edgar <ecastillejossantamaria@gmail.com>
2021-05-14 18:22:26 -0500

    prototype report's front

--- client/front-office/src/components/reports/Reports.js
+++ client/front-office/src/components/reports/Reports.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
 export default function Reports() {
-  return <div>Reports dentro</div>;
+  return (
+    <div>
+      <div className="bg-warning d-flex justify-content-between w-100 p-3 rounded-top">
+        <h5>Download your movements reports </h5>
+      </div>
+      <div className="d-flex w-100 justify-content-center p-5">
+        <button type="button" className="btn btn-block btn-info btn-xs w-25">
+          <h6>Download all movements</h6>
+        </button>
+      </div>
+      <div className="d-flex justify-content-center">Or</div>
+      <div className="d-flex justify-content-center p-5">
+        <label>Filter by:</label>
+        <select name="select">
+          <option value="value1" selected disabled>
+            None
+          </option>
+          <option value="value1">Type</option>
+          <option value="value2">Date</option>
+          <option value="value3">Wallet</option>
+        </select>
+      </div>
+      /api/fo/
+    </div>
+  );
 }
