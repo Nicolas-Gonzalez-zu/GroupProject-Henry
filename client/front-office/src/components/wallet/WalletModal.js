89d6a07bc52f449c187971cc43ae0ff93230814e
joacobian21@gmail.com <joacobian21@gmail.com>
2021-05-16 16:12:50 -0300

    adding some validations to wallet

--- client/front-office/src/components/wallet/WalletModal.js
+++ client/front-office/src/components/wallet/WalletModal.js
@@ -3,11 +3,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Modal, Button } from 'react-bootstrap';
 import { useFormik } from 'formik';
 import Swal from 'sweetalert2';
+import NoAddWalletModal from './example';
 import * as action from '../../actions/creators';
 
 const WalletModal = () => {
   const [showModal, setShowModal] = useState(false);
   const authAlert = useSelector((state) => state.authReducers.authAlert);
+  const wallets = useSelector((state) => state.walletReducer.wallets);
   const dispatch = useDispatch();
   const setModalHandler = () => {
     setShowModal(!showModal);
@@ -32,6 +34,7 @@ const WalletModal = () => {
         }
       });
     }
+    action.getWallet(dispatch);
   }, [dispatch, authAlert.fire, authAlert.message, authAlert.type]);
 
   const validate = (values) => {
@@ -64,73 +67,80 @@ const WalletModal = () => {
       }, 1500);
     },
   });
+
+  const walletsAvailable = wallets.filter((w) => w.status);
   return (
     <div>
+      {console.log(walletsAvailable)}
       <Button onClick={setModalHandler} className="btn-success">
         Add Wallet
       </Button>
-      <Modal show={showModal}>
-        <Modal.Header className="d-flex flex-column bg-info">
-          <Button className="btn btn-danger align-self-end" onClick={setModalHandler}>
-            X
-          </Button>
-          <h4>Complete the inputs please</h4>
-        </Modal.Header>
-        <Modal.Body>
-          <form
-            className="d-flex flex-column justify-content-center"
-            onSubmit={formik.handleSubmit}
-          >
-            <div className="d-flex flex-column m-3">
-              <label className="align-self-center">Name</label>
-              <input
-                type="text"
-                name="name"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                placeholder="cash..."
-                autoComplete="off"
-                className={
-                  formik.errors.name
-                    ? 'form-control is-invalid w-50 align-self-center'
-                    : 'form-control w-50 align-self-center'
-                }
-              />
-              {formik.errors.name ? (
-                <p className="text text-danger align-self-center">{formik.errors.name}</p>
-              ) : (
-                ''
-              )}
-            </div>
-            <div className="d-flex flex-column m-3">
-              <label className="align-self-center">Balance</label>
-              <input
-                type="text"
-                name="balance"
-                value={formik.values.balance}
-                onChange={formik.handleChange}
-                placeholder="300..."
-                autoComplete="off"
-                className={
-                  formik.errors.balance
-                    ? 'form-control is-invalid w-50 align-self-center'
-                    : 'form-control w-50 align-self-center'
-                }
-              />
-              {formik.errors.balance ? (
-                <p className="text text-danger align-self-center">{formik.errors.balance}</p>
-              ) : (
-                ''
-              )}
-            </div>
-            <div className="d-flex justify-content-center">
-              <Button type="submit" className="btn btn-success">
-                Add wallet
-              </Button>
-            </div>
-          </form>
-        </Modal.Body>
-      </Modal>
+      {walletsAvailable.length >= 10 ? (
+        <NoAddWalletModal showModal={showModal} setModalHandler={setModalHandler} />
+      ) : (
+        <Modal show={showModal}>
+          <Modal.Header className="d-flex flex-column bg-info">
+            <Button className="btn btn-danger align-self-end" onClick={setModalHandler}>
+              X
+            </Button>
+            <h4>Complete the inputs please</h4>
+          </Modal.Header>
+          <Modal.Body>
+            <form
+              className="d-flex flex-column justify-content-center"
+              onSubmit={formik.handleSubmit}
+            >
+              <div className="d-flex flex-column m-3">
+                <label className="align-self-center">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  placeholder="cash..."
+                  autoComplete="off"
+                  className={
+                    formik.errors.name
+                      ? 'form-control is-invalid w-50 align-self-center'
+                      : 'form-control w-50 align-self-center'
+                  }
+                />
+                {formik.errors.name ? (
+                  <p className="text text-danger align-self-center">{formik.errors.name}</p>
+                ) : (
+                  ''
+                )}
+              </div>
+              <div className="d-flex flex-column m-3">
+                <label className="align-self-center">Balance</label>
+                <input
+                  type="text"
+                  name="balance"
+                  value={formik.values.balance}
+                  onChange={formik.handleChange}
+                  placeholder="300..."
+                  autoComplete="off"
+                  className={
+                    formik.errors.balance
+                      ? 'form-control is-invalid w-50 align-self-center'
+                      : 'form-control w-50 align-self-center'
+                  }
+                />
+                {formik.errors.balance ? (
+                  <p className="text text-danger align-self-center">{formik.errors.balance}</p>
+                ) : (
+                  ''
+                )}
+              </div>
+              <div className="d-flex justify-content-center">
+                <Button type="submit" className="btn btn-success">
+                  Add wallet
+                </Button>
+              </div>
+            </form>
+          </Modal.Body>
+        </Modal>
+      )}
     </div>
   );
 };
