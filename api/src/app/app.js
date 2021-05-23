bfb3e946a891e67b11fe03a60ac480d0be7370c9
Yilmer Daniel Avila Villarreal <yilmer0001@hotmail.com>
2021-05-23 17:05:46 -0500

    add ipn routes

--- api/src/app/app.js
+++ api/src/app/app.js
@@ -3,14 +3,14 @@ const logger = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const passport = require('./auth/setup');
 
-const corsOptions = {
-  credentials: true,
-  origin: process.env.ALLOWED_ORIGIN || 'http://localhost:3000',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const corsOptions = {
+//   credentials: true,
+//   origin: process.env.ALLOWED_ORIGIN || 'http://localhost:3000',
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
 const router = require('./routes');
 
@@ -44,7 +44,20 @@ app.use(
 app.use(passport.initialize({}));
 app.use(passport.session({}));
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  // const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000'];
+  const { origin } = req.headers;
+  // if (allowedOrigins.includes(origin)) {
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  // }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access-control-allow-origin',
+  );
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
 app.use('/api', router);
 
 module.exports = app;
