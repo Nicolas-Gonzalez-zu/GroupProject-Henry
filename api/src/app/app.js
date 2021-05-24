4a6a4b4b130b965cbbea04426f4e6170a47d9814
Yilmer Daniel Avila Villarreal <yilmer0001@hotmail.com>
2021-05-23 20:59:52 -0500

    Fix cors problems

--- api/src/app/app.js
+++ api/src/app/app.js
@@ -3,16 +3,30 @@ const logger = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const passport = require('./auth/setup');
+
+const corsOptions = {
+  credentials: true,
+  origin: true, // ['http://localhost:3001'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 const router = require('./routes');
 
 const { MONGO_URI } = process.env;
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
 // logging middleware
 app.use(logger('dev'));
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  next();
+});
 
 // bodyparse middlewares
 app.use(express.json());
@@ -24,7 +38,7 @@ app.use(
   session({
     secret: 'very secret key',
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     httpOnly: true,
     store: MongoStore.create({ mongoUrl: MONGO_URI }),
     sameSite: true,
@@ -35,19 +49,7 @@ app.use(
 app.use(passport.initialize({}));
 app.use(passport.session({}));
 
-app.use((req, res, next) => {
-  // const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000'];
-  console.log(req.headers);
-  const { origin, host } = req.headers;
-  res.setHeader('Access-Control-Allow-Origin', origin || host);
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE, OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, access-control-allow-origin',
-  );
-  res.header('Access-Control-Allow-Credentials', true);
-  return next();
-});
+app.use(cors(corsOptions));
 app.use('/api', router);
 
 module.exports = app;
