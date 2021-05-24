ffa3c14449d75540b7260e77e921eb7bc2f4f6aa
Yilmer Daniel Avila Villarreal <yilmer0001@hotmail.com>
2021-05-23 20:05:03 -0500

    fix origins error

--- api/src/app/app.js
+++ api/src/app/app.js
@@ -3,19 +3,10 @@ const logger = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const cookieParser = require('cookie-parser');
-// const cors = require('cors');
 const passport = require('./auth/setup');
-
-// const corsOptions = {
-//   credentials: true,
-//   origin: process.env.ALLOWED_ORIGIN || 'http://localhost:3000',
-//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
-
 const router = require('./routes');
 
 const { MONGO_URI } = process.env;
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -46,12 +37,10 @@ app.use(passport.session({}));
 
 app.use((req, res, next) => {
   // const allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000'];
-  const { host } = req.headers;
-  console.log('Origin: ', host);
-  // if (allowedOrigins.includes(origin)) {
-  res.setHeader('Access-Control-Allow-Origin', host);
-  // }
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE');
+  console.log(req.headers);
+  const { origin, host } = req.headers;
+  res.setHeader('Access-Control-Allow-Origin', origin || host);
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, PUT, POST, DELETE, OPTIONS');
   res.header(
     'Access-Control-Allow-Headers',
     'Content-Type, Authorization, access-control-allow-origin',
